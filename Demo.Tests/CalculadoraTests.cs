a54fc0b7247bdea7016225a47ee96fbefc53c852
André Dantas <dededobem@gmail.com>
2021-03-24 23:37:22 -0300

    Adicionar arquivos de projeto.

---
+++ Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(2, 2);
+
+            //Assert
+            Assert.Equal(4, resultado);
+
+        }
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(3, 3, 6)]
+        [InlineData(2, 5, 7)]
+        [InlineData(3, 8, 11)]
+        [InlineData(4, 1, 5)]
+        public void Calculadora_Somar_RetornarValoresDiversosDaSoma(double v1, double v2, double resultado)
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var result = calculadora.Somar(v1, v2);
+
+            //Assert
+            Assert.Equal(result, resultado);
+        }
+    }
+}
---
+++ Demo.Tests/StringsToolsTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class StringsToolsTests
+    {
+        [Fact]
+        public void StringsTools_UnirNomes_RetornarNomeCompleto()
+        {
+            //Arrange
+            var unirStr = new StringsTools();
+
+            //Act
+            var nomeCompleto = unirStr.Unir("André", "Dantas");
+
+            //Assert
+            Assert.Equal("André Dantas", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_IgnoreCase()
+        {
+            //Arrange 
+            var unirStr = new StringsTools();
+
+            //Act
+            var nomeCompleto = unirStr.Unir("André", "Dantas");
+
+            //Assert
+            Assert.Equal("ANDRÉ DANTAS", nomeCompleto, true);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveConterTrecho()
+        {
+            //Arrange
+            var unirStr = new StringsTools();
+
+            //Act
+            var nomeCompleto = unirStr.Unir("André", "Dantas");
+
+            //Assert
+            Assert.Contains("ndr", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveComecarCom()
+        {
+            //Arrange
+            var unirStr = new StringsTools();
+
+            //Act
+            var nomeCompleto = unirStr.Unir("André", "Dantas");
+
+            //Assert
+            Assert.StartsWith("And", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveAcabarCom()
+        {
+            //Arrange
+            var unirStr = new StringsTools();
+
+            //Act
+            var nomeCompleto = unirStr.Unir("André", "Dantas");
+
+            //Assert
+            Assert.StartsWith("And", nomeCompleto);
+        }
+    }
+}
