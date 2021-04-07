dc8887ed9743425b01436e2c7255c69f03f91ad6
André Dantas <dededobem@gmail.com>
2021-04-07 00:09:06 -0300

    Inserção framework bogus

---
+++ Recursos.Tests/4-DadosHumanos/ClienteBogusTests.cs
@@ -0,0 +1,48 @@
+﻿using Recursos.Tests._2_Fixtures;
+using Xunit;
+
+namespace Recursos.Tests._4_DadosHumanos
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteBogusTests
+    {
+        private readonly ClienteTestsBogusFixture _clienteTestsFixture;
+
+        public ClienteBogusTests(ClienteTestsBogusFixture clienteTestsFixture)
+        {
+            _clienteTestsFixture = clienteTestsFixture;
+        }
+
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Bogus Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            //Arrange
+            var cliente = _clienteTestsFixture.GerarClienteValido();
+
+            //Act
+            var result = cliente.EhValido();
+
+            //Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Bogus Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            //Arrange
+            var cliente = _clienteTestsFixture.GerarClienteValido();
+
+            //Act
+            var result = cliente.EhValido();
+
+            //Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+
+        }
+    }
+}
---
+++ Recursos.Tests/4-DadosHumanos/ClienteTestsBogusFixture.cs
@@ -0,0 +1,57 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using Recursos.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Recursos.Tests._4_DadosHumanos
+{
+
+    [CollectionDefinition(nameof(ClienteBogusCollection))]
+    public class ClienteBogusCollection : ICollectionFixture<ClienteTestsBogusFixture> { }
+
+    public class ClienteTestsBogusFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(80, DateTime.Now.AddYears(-18)),
+                    DateTime.Now,
+                    "",
+                    true))
+                .RuleFor(c => c.Email, (f, c) =>
+                    f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInvalido() {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(1, DateTime.Now.AddYears(1)),
+                    DateTime.Now,
+                    "",
+                    false));
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
+
